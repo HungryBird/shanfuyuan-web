8b251f35f0732fd173863c5c6e3a73f3a1413a30
覃辉 <qinhuiherospeed@dingtalk.com>
2020-01-16 18:05:41 +0800

    完成个人中心布局

---
+++ pages/user/config.js
@@ -0,0 +1,32 @@
+export default {
+  menu: [
+    {
+      icon: require('../../static/img/user/wodeyue.png'),
+      title: '我的余额'
+    },
+    {
+      icon: require('../../static/img/user/wodejifen.png'),
+      title: '我的积分'
+    },
+    {
+      icon: require('../../static/img/user/wodefenyong.png'),
+      title: '我的分佣'
+    },
+    {
+      icon: require('../../static/img/user/wodeshoucang.png'),
+      title: '我的收藏'
+    },
+    {
+      icon: require('../../static/img/user/yaoqinghaoyou.png'),
+      title: '邀请好友'
+    },
+    {
+      icon: require('../../static/img/user/xitongshezhi.png'),
+      title: '系统设置'
+    },
+    {
+      icon: require('../../static/img/user/lianxikefu.png'),
+      title: '联系客服'
+    }
+  ]
+}
