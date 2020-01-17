6192cb82727fedfc74ab2bd67fcf7f56365c1a69
何俊洁 <381548325@qq.com>
2020-01-17 10:57:15 +0800

    :sparkles: 祭拜person

--- pages/pray/pray.vue
+++ pages/pray/pray.vue
@@ -1,30 +1,20 @@
 <template>
     <view class="content" :style="{'padding-bottom': paddingBottom + 'px'}">
-        <!-- <view class="hello">
-            <view class="title">
-                您好 {{userName}}，您已成功登录。
-            </view>
-            <view class="ul">
-                <view>这是 uni-app 带登录模板的示例App首页。</view>
-                <view>在 “我的” 中点击 “退出” 可以 “注销当前账户”</view>
-            </view>
-        </view> -->
 		<view class="main">
-			<view class="good">
-				<!-- <img :src="godDict[god.id][img]"  /> -->
-				{{ godDict[god.id] }}
+			<view class="god">
+				<img src="../../static/img/pray/dabeiguang.png" >
+				<img :src="godDict[god.id]['img']"  />
+			</view>
+			<view class="cushion-wrap">
+				<view class="person" v-if='praying'>
+					<img :src="animation.person" />
+				</view>
+				<view class="cushion" @click="goPray">
+					<img src="../../static/img/pray/guidian.png" />
+				</view>
 			</view>
 		</view>
-        <!-- <view v-if="!hasLogin" class="hello">
-            <view class="title">
-                您好 游客。
-            </view>
-            <view class="ul">
-                <view>这是 uni-app 带登录模板的示例App首页。</view>
-                <view>在 “我的” 中点击 “登录” 可以 “登录您的账户”</view>
-            </view>
-        </view> -->
-		<tab-bar :active="2" ref="tab"></tab-bar>
+		<tab-bar :active="2" ref="tab" class="tab"></tab-bar>
     </view>
 </template>
 
@@ -39,6 +29,10 @@
     export default {
 		data() {
 			return {
+				praying: false,	// 是否祭拜中
+				animation: {
+					person: '',
+				},
 				paddingBottom: 0,
 				godDict: {
 					1: {
@@ -76,13 +70,10 @@
 			// 
         },
 		onResize() {
-			this.setPaddingBottom();
+			// this.setPaddingBottom();
 		},
 		onReady() {
-			this.setPaddingBottom();
-			console.log('getUniStorageSync: ', getUniStorageSync('god'))
-			console.log('god: ', this.god)
-			console.log('token: ', this.token);
+			// this.setPaddingBottom();
 		},
 		methods:{
 			// 设置padding-bottom
@@ -90,22 +81,111 @@
 				setTimeout(() => {
 					this.$nextTick(function(){
 						this.paddingBottom = this.$refs.tab.$el.offsetHeight;
+						console.log('this.paddingBottom: ', this.paddingBottom)
 					})
 				}, 0)
+			},
+			// 开始祭拜
+			goPray() {
+				if(this.praying) return;
+				this.praying = true;
+				// 拜了几次
+				let count = 0;
+				// 当前图片是哪个
+				let index = 1;
+				const self = this;
+				let target = '../../static/img/pray/person/';
+				// 填补计时器延时执行
+				self.animation.person = '../../static/img/pray/person/m1.png';
+				let timer = setInterval(() => {
+					if(count > 2) {
+						clearInterval(timer)
+						self.praying = false;
+						return;
+					}
+					// m:male, f:famle
+					self.animation.person = `${target}m${index}.png`;
+					if(index === 5) {
+						index = 1;
+						count++;
+					}
+					else {
+						index++;
+					}
+				}, 350)
 			}
 		}
     }
 </script>
 
-<style>
+<style lang="scss">
 	.content{
 		padding: 0;
+		display: flex;
+		flex-direction: column;
+		.tab{
+			position: relative !important;
+		}
 	}
 	.main{
+		position: relative;
 		width: 100%;
 		height: 100%;
 		background: url(../../static/img/pray/jibaibeijing.png) no-repeat center center;
 		background-size: cover;
+		flex: 1;
+		.god{
+			position: relative;
+			width: 70%;
+			margin: 0 auto;
+			top: 40upx;
+			img{
+				&:first-child{
+					display: block;
+					position: absolute;
+					top: -80upx;
+					left: 50%;
+					transform: translateX(-50%);
+					z-index: 0;
+					animation: scaleLight 2s;
+					animation-timing-function: ease-in-out;
+					animation-iteration-count: infinite;
+				}
+				&:nth-child(2){
+					position: relative;
+					width: 100%;
+					object-fit: cover;
+					z-index: 1;
+				}
+			}
+		}
+		.cushion-wrap{
+			position: absolute;
+			bottom: 0;
+			left: 50%;
+			transform: translateX(-50%);
+			.person{
+				position: relative;
+				width: 40%;
+				top: 60upx;
+				z-index: 99999999;
+				margin: 0 auto;
+				img{
+					display: block;
+					width: 100%;
+					object-fit: cover;
+				}
+			}
+			.cushion{
+				width: 40%;
+				margin: 0 auto;
+				img{
+					display: block;
+					width: 100%;
+					object-fit: cover;
+				}
+			}
+		}
 	}
     .hello {
         display: flex;
@@ -127,4 +207,16 @@
     .ul>view {
         line-height: 50upx;
     }
+	@keyframes scaleLight
+	{
+		0{
+			transform: translateX(-50%) scale(1);
+		}
+		50%{
+			transform: translateX(-50%) scale(0.8);
+		}
+		100%{
+			transform: translateX(-50%) scale(1);
+		}
+	}
 </style>
